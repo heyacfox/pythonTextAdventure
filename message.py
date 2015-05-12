b56972e25176e8f8b42933c7e9cd7c65260a4797
heyacfox <heyacfox@gmail.com>
2015-05-12 13:21:22 -0400

    Cleaned up Files, added More Files
    Implemented messaging and the gameWorldInterface class. The gameWorld is
    crucial here. I also created the "runGame" module for our testing
    purposes. In order to spin up a working game, we need to pass the
    gameWorldInterface to both the server and the game code.

---
+++ message.py
@@ -0,0 +1,21 @@
+#This class defines the messages that
+#get sent from the client to the server,
+#and from the server to the client.
+
+#My job is to send things. It's everyone else's
+#problem to see what comes out of it
+
+#Note: Because python, the message can literally be anything.
+#I can take it objects if we want, but currently
+#I will only plan to handle string messages.
+
+#I don't know what the Id system we want to use looks like yet.
+#Whatever we choose, server has to convert it to port addresses anyway
+
+class message:
+
+    def __init__(self, newFromUserId, newToUserId, newMessage):
+        self.fromUserId = newFromUserId
+        self.toUserId = newToUserId
+        self.stringMessage = newMessage
+        
---
+++ networkClient.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+"""
+An echo client that allows the user to send multiple lines to the server.
+Entering a blank line will exit the client.
+"""
+
+#This code was found at http://ilab.cs.byu.edu/python/threadingmodule.html
+
+#taking some code from http://bytes.com/topic/python/answers/43936-canceling-interrupting-raw_input
+
+import socket
+import sys
+import threading
+from threading import Thread
+
+host = 'localhost'
+port = 50007
+size = 1024
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host,port))
+sys.stdout.write('%')
+"""
+while 1:
+    # read from keyboard
+    line = input("What do you do?\n>> ").encode('UTF-8')
+    if line == '\n':
+        break
+    s.send(line)
+    data = s.recv(size)
+    sys.stdout.write(data.decode('UTF-8'))
+    sys.stdout.write('%>> ')
+s.close()
+"""
+class receiveLoop(threading.Thread):
+    def __init__(self):
+        self.run()
+
+    def run(self):
+        while 1:
+            data = s.recv(size)
+            if data:
+                sys.stdout.write(data.decode('UTF-8'))
+
+def inputLoop():
+    while 1:
+        line=input("What do you do?\n>> ").encode('UTF-8')
+        s.send(line)
+
+mythread = receiveLoop()
+receiveLoop.start()
+
+
+inputLoop()
