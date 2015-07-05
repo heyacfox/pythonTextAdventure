d66205a84eb7b5e70aec4706ea1267158a8c706f
heyacfox <heyacfox@gmail.com>
2015-07-05 12:42:14 -0400

    Broke the server, broke the fake world, it was a bad day

--- RunServerAndFakeWorldTest.py
+++
@@ -1,9 +0,0 @@
-import tornadoServerTest
-import faked_game_world
-import faked_game_world_2
-import gameWorldInterface
-import threading
-
-gw2 = faked_game_world_2.GameWorld()
-
-#run tornadoServerTest.beginServer(gw2) to run
--- message.py
+++ message.py
@@ -2,20 +2,21 @@
 #get sent from the client to the server,
 #and from the server to the client.
 
-#My job is to send things. It's everyone else's
-#problem to see what comes out of it
-
-#Note: Because python, the message can literally be anything.
-#I can take it objects if we want, but currently
-#I will only plan to handle string messages.
-
 #I don't know what the Id system we want to use looks like yet.
 #Whatever we choose, server has to convert it to port addresses anyway
 
 class Message:
 
-    def __init__(self, newFromUserId, newToUserId, newMessage):
-        self.fromUserId = newFromUserId
-        self.toUserId = newToUserId
-        self.stringMessage = newMessage
+    def __init__(self, new_from_user_id, new_to_user_id, new_message):
+        #A string: Who the message was sent from. Can be a user id,
+        #but is also often "server" or "world". 
+        self.from_user_id = new_from_user_id
+        #The intended recipient of the message. Can be a user id, but can
+        #also be "server" or "world". Use "server" when sending from the
+        #world to indicate a system message, and use "world" when sending
+        #from the server to indicate that a user typed in something
+        #that was not specifically directed towards anyone.
+        self.to_user_id = new_to_user_id
+        #The string content of the message
+        self.string_message = new_message
         
